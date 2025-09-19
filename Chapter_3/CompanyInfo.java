148db0e6cf8a870eb33f4b84cd1c51a75308a5f4
IvanJaeMandawee-UCM <ivanjaemandawe@gmail.com>
2025-09-19 08:43:54 +0800

    Add files via upload

---
+++ Chapter_3/CompanyInfo.java
@@ -0,0 +1,24 @@
+  
+  
+   
+      
+        public class CompanyInfo {
+        
+            public static void main(String [] args) {
+               //dispay
+                  displayHours();
+                  
+                   
+            }
+            
+            public static void displayHours() {
+                System.out.print("Monday: 07:30 AM - 06:30 PM");
+                 System.out.print("\nTuesday: 07:00 AM - 05:30 PM");
+                  System.out.print("\nWednesay: 09:00 AM - 05:30 PM");
+                   System.out.print("\nThuresday: 08:30 AM - 07:00 PM");
+                    System.out.print("\nFriday: 08:30 AM - 05:00 PM");     
+                    System.out.print("\nSaturday: 09:00 AM - 05:30 PM");
+                   System.out.print("\nClose");
+          
+              }
+                  }
